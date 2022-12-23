71069ccb3abefe270ec2dee387281a409ac07e04
iremsmr <iremsamur129@gmail.com>
2022-12-23 21:00:31 +0300

    Rapid Api is added

---
+++ UpSchool_Api_Consume/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UpSchool_Api_Consume.Models;
+using Newtonsoft.Json;
+
+namespace UpSchool_Api_Consume.Controllers
+{
+    public class MovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<MovieListViewModel> movies = new List<MovieListViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "de4f9922d2msh4b0e087ebed5975p1aba80jsn10aeb0527b81" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                movies = JsonConvert.DeserializeObject<List<MovieListViewModel>>(body);
+                return View(movies);
+            }
+      
+        }
+    }
+}
---
+++ UpSchool_Api_Consume/Models/MovieListViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace UpSchool_Api_Consume.Models
+{
+    public class MovieListViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string thumbnail { get; set; }
+        public string rating { get; set; }
+        public string id { get; set; }
+        public int year { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public string trailer { get; set; }
+        public string[] genre { get; set; }
+        public string[] director { get; set; }
+        public string[] writers { get; set; }
+        public string imdbid { get; set; }
+    }
+}
